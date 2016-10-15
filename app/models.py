e54fc1a4b04d655130c3d09a373103150ed414db
Kit Roed <kit.roed@gmail.com>
2016-10-14 23:27:57 -0500

    hash being generated is too long for 64 char? #15

--- app/models.py
+++ app/models.py
@@ -14,7 +14,7 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(32), index=True)
-    password_hash = db.Column(db.String(64))
+    password_hash = db.Column(db.String(128))
     machines = db.relationship('Machine', backref='author', lazy='dynamic')
 
     def hash_password(self, password):
