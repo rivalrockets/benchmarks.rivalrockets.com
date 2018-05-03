28545c89d655a4d800dc6e7fc8a70305f3d5dfe7
Kit Roed <kit.roed@gmail.com>
2018-05-03 00:07:32 -0500

    Upgrade to Flask 1.0.1, switch from Flask-Script to Flask CLI.

--- rivalrockets-api.py
+++ rivalrockets-api.py
@@ -1,30 +1,23 @@
-#!/usr/bin/env python
 import os
 from app import create_app, db
 from app.models import User, Machine, Revision
-from flask_script import Manager, Shell
-from flask_migrate import Migrate, MigrateCommand
+from flask_migrate import Migrate
+import click
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
-manager = Manager(app)
 migrate = Migrate(app, db)
 
 
+@app.shell_context_processor
 def make_shell_context():
     return dict(app=app, db=db, User=User, Machine=Machine,
                 Revision=Revision)
 
-manager.add_command("shell", Shell(make_context=make_shell_context))
-manager.add_command("db", MigrateCommand)
 
-@manager.command
+@app.cli.command()
 def deploy():
     """Run deployment tasks."""
     from flask_migrate import upgrade
-    
+
     # migrate database to latest Revision
     upgrade()
-
-
-if __name__ == '__main__':
-    manager.run()
